75ca25f6b64d3600fde2e4310b146e14c18445a9
Kevin M. Smith <kevin.smith@tufts.edu>
2015-03-22 14:32:31 -0400

    Fix multiple trigger bug with off()

--- www/js/main.js
+++ www/js/main.js
@@ -59,14 +59,14 @@ require(['manifiesto', 'jquery', 'jquerymobile'], function(manifiesto, $, jqm) {
             function() {
                 $(this).on("click", ".info-go", function (e) {
                     e.preventDefault();
+                    $("#pagina-rejilla-de-familias").data("orden", ordenes[this.id]);
+                    $("#pagina-lista-de-familias").data("orden", ordenes[this.id]);
 
                     if(ordenes[this.id]['familias'].length == 1){
                         $("#pagina-de-familia").data("familia", ordenes[this.id]['familias'][0]);
+                        $("#pagina-de-familia").data("pagina-anterior", '#pagina-lista-de-ordenes')
                         $.mobile.changePage("#pagina-de-familia");
-
                     } else {
-                        $("#pagina-rejilla-de-familias").data("orden", ordenes[this.id]);
-                        $("#pagina-lista-de-familias").data("orden", ordenes[this.id]);
                         $.mobile.changePage("#pagina-lista-de-familias");
                     }
                 });
@@ -81,27 +81,22 @@ require(['manifiesto', 'jquery', 'jquerymobile'], function(manifiesto, $, jqm) {
     $( document ).on("pagecreate", "#pagina-rejilla-de-ordenes", function() {
         var _grid = '';
         $.each(ordenes, function(i, _orden) {
-            if(i % 2 == 0 ){
-                _grid = _grid + '<div class="ui-block-a"><a href="#" class="info-go" id="' + i + '"><img src="img/icones/' + _orden.nombre + '.png"></a></div>'
-
-                // _grid = _grid + '<div class="ui-block-a">' + _orden.nombre + '</div>'
-            } else {
-                _grid = _grid + '<div class="ui-block-a"><a href="#" class="info-go" id="' + i + '"><img src="img/icones/' + _orden.nombre + '.png"></a></div>'
-
-                // _grid = _grid + '<div class="ui-block-b">' + _orden.nombre + '</div>'
-            }
+                _grid += '<div class="ui-block-a"><a href="#" class="info-go" id="'
+                + i + '"><img src="img/icones/'
+                + _orden.nombre + '.png"></a></div>';
         });
 
         $("#rejilla-de-ordenes").append(_grid).promise().done(
             function() {
                 $(this).on("click", ".info-go", function (e) {
+                    e.preventDefault();
+                    $("#pagina-rejilla-de-familias").data("orden", ordenes[this.id]);
+                    $("#pagina-lista-de-familias").data("orden", ordenes[this.id]);
                     if (ordenes[this.id]['familias'].length == 1) {
                         $("#pagina-de-familia").data("familia", ordenes[this.id]['familias'][0]);
+                        $("#pagina-de-familia").data("pagina-anterior", '#pagina-rejilla-de-ordenes')
                         $.mobile.changePage("#pagina-de-familia");
                     } else {
-                        e.preventDefault();
-                        $("#pagina-rejilla-de-familias").data("orden", ordenes[this.id]);
-                        $("#pagina-lista-de-familias").data("orden", ordenes[this.id]);
                         $.mobile.changePage("#pagina-rejilla-de-familias");
                     }
                 });
@@ -130,12 +125,14 @@ require(['manifiesto', 'jquery', 'jquerymobile'], function(manifiesto, $, jqm) {
             li += '<li><a href="#" class="info-go" id="' + i + '">' + _familia.nombre + '</a></li>'
         });
 
+        $("#lista-de-familias").off()
         $("#lista-de-familias").html(li).promise().done(
             function() {
 
                 $(this).on("click", ".info-go", function (e) {
                     e.preventDefault();
-                    $("#pagina-de-familia").data("familia", familias[this.id]);
+                    $("#pagina-de-familia").data("familia", familias[this.id])
+                    $("#pagina-de-familia").data("pagina-anterior", '#pagina-lista-de-familias')
                     $.mobile.changePage("#pagina-de-familia");
                 });
 
@@ -149,31 +146,29 @@ require(['manifiesto', 'jquery', 'jquerymobile'], function(manifiesto, $, jqm) {
      ***************************************/
 
     $(document).on("pagebeforeshow", "#pagina-rejilla-de-familias", function () {
-        //get from data - you put this here when the "a" wa clicked in the previous page
         var orden = $(this).data("orden");
         var familias = orden['familias'];
         var _grid = "";
 
         $("#encabezado-pagina-rejilla-de-familias").text(orden.nombre)
 
-        $.each(orden['familias'], function(i, _familia){
-            if(i % 2 == 0 ){
-                _grid = _grid + '<div class="ui-block-a"><a href="#" class="info-go" id="' + i + '"><h3>' + _familia.nombre + '</h3><img src="' + _familia.fotos[0] + '"></a><hr></div>'
-
-                // _grid = _grid + '<div class="ui-block-a">' + _orden.nombre + '</div>'
-            } else {
-                _grid = _grid + '<div class="ui-block-a"><a href="#" class="info-go" id="' + i + '"><h3>' + _familia.nombre + '</h3><img src="' + _familia.fotos[0] + '"></a><hr></div>'
-                // _grid = _grid + '<div class="ui-block-b">' + _orden.nombre + '</div>'
-            }
+        $.each(familias, function(i, _familia){
+                _grid += '<div class="ui-block-a"><a href="#" class="info-go" id="'
+                + i + '"><h3>'
+                + _familia.nombre
+                + '</h3><img src="'
+                + _familia.fotos[0]
+                + '"></a><hr></div>';
         });
 
+        $("#rejilla-de-familias").off()
         $("#rejilla-de-familias").html(_grid).promise().done(
             function() {
-
                 $(this).on("click", ".info-go", function (e) {
                     e.preventDefault();
                     console.log(this);
                     $("#pagina-de-familia").data("familia", familias[this.id]);
+                    $("#pagina-de-familia").data("pagina-anterior", '#pagina-rejilla-de-familias')
                     $.mobile.changePage("#pagina-de-familia");
 
                 });
@@ -183,35 +178,33 @@ require(['manifiesto', 'jquery', 'jquerymobile'], function(manifiesto, $, jqm) {
     });
 
     /***************************************
-     *	preparar página 'rejilla-de-familia'
+     *	preparar página de familia
      ***************************************/
 
     $(document).on("pagebeforeshow", "#pagina-de-familia", function() {
-       var familia = $(this).data("familia")
-        var fotos = familia['fotos']
-        console.log(fotos)
+        var familia = $(this).data("familia");
+        var fotos = familia['fotos'];
+        var pagina_anterior = $(this).data("pagina-anterior")
         var _grid = "";
 
-
         $("#encabezado-pagina-de-familia").text(familia.nombre + ' (' + familia.puntos + ')');
+        $("#boton-atras-pagina-de-familia").attr('href', pagina_anterior)
 
         $.each(familia['fotos'], function(i, foto){
-            if(i % 2 == 0 ){
-                _grid = _grid + '<div class="ui-block-a"><a href="#" class="info-go" id="' + i + '"><img src="' + foto + '"></a><hr></div>'
-            } else {
-                _grid = _grid + '<div class="ui-block-a"><a href="#" class="info-go" id="' + i + '"><img src="' + foto + '"></a><hr></div>'
-            }
+                _grid += '<div class="ui-block-a"><a href="#" class="info-go" id="'
+                + i + '"><img src="'
+                + foto + '"></a><hr></div>';
         });
 
+
+        $("#rejilla-de-familia").off()
         $("#rejilla-de-familia").html(_grid).promise().done(
             function() {
-
                 $(this).on("click", ".info-go", function (e) {
                     e.preventDefault();
                     $("#pagina-foto").data("foto", fotos[this.id]);
                     $("#pagina-foto").data("nombre", familia['nombre']);
                     $.mobile.changePage("#pagina-foto");
-
                 });
 
             });
@@ -220,12 +213,15 @@ require(['manifiesto', 'jquery', 'jquerymobile'], function(manifiesto, $, jqm) {
 
 
     $(document).on("pagebeforeshow", "#pagina-foto", function() {
-    var foto = $(this).data("foto")
-    var nombre = $(this).data("nombre")
-        console.log(foto)
-
-        $("#encabezado-pagina-foto").text(nombre)
-        $("#foto-grande").attr('src', foto)
+        var foto = $(this).data("foto");
+        var nombre = $(this).data("nombre");
+        $("#encabezado-pagina-foto").text(nombre);
+        $("#foto-grande").attr('src', foto);
+        
+        $("#boton-atras-pagina-foto").off()
+        $("#boton-atras-pagina-foto").on("click", function(e) {
+            $.mobile.changePage("#pagina-de-familia")
+        });
 
     });
 
